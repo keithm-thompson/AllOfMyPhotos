e61ec088ab51d03e1dab0da3277abf2696de6ff6
Keith Thompson <keithm_thompson@outlook.com>
2016-10-14 07:09:37 -0400

    finish adding handlers to feed

--- frontend/components/feed/feed_photo.jsx
+++ frontend/components/feed/feed_photo.jsx
@@ -4,7 +4,7 @@ export default ({ photo, clickPhoto, clickUser }) => (
   <li className="photo-container">
     <img src={ photo.image_url } className="feed-photo" onClick={ clickPhoto }/>
     <div>
-      <img src={ photo.user.image_url } className="feed-user-icon" />
+      <img src={ photo.user.image_url } className="feed-user-icon" onClick={ clickUser } />
       <div className="photo-info" onClick={ clickUser }>
         <h4>{ photo.user.username }</h4>
         <h5 className="uploaded-time" >{ photo.time_since_uploaded } ago</h5>
